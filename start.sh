e9462f33444b4520752bec70d599cb5fa127ea56
Luis Saravia <saraviatornero310@gmail.com>
2024-10-22 18:27:06 -0500

    fix(project): delete comments and properties "build"

--- start.sh
+++ start.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-git clone https://github.com/saravia-developer/api-login-auth.git &&
-git clone https://github.com/saravia-developer/login-angular.git &&
-
 docker compose -f docker-compose.yml up -d --build
