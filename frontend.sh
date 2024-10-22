c65657ace59ee1035196308d9e0ad4cfe7f959d3
Luis Saravia <saraviatornero310@gmail.com>
2024-10-22 13:37:23 -0500

    fix(project): add healthcheck/docker-compose,delete back & front

--- frontend.sh
+++
@@ -1,9 +0,0 @@
-# mkdir frontend;
-# cd frontend;
-# git clone https://github.com/saravia-developer/login-angular.git;
-
-# sleep 25;
-
-# cd login-angular;
-# npm install;
-# npm start
--- start.sh
+++ start.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# echo "Creando el backend y base de datos";
-# sh backend.sh &
-
-# echo "Creando el frontend";
-# sh frontend.sh &
+git clone https://github.com/saravia-developer/api-login-auth.git &&
+git clone https://github.com/saravia-developer/login-angular.git &&
 
 docker compose -f docker-compose.yml up -d --build
