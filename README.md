9ff31595de5347267e3fe51bc62d57d53b9a8a91
Luis Saravia <saraviatornero310@gmail.com>
2024-10-26 12:42:16 -0500

    feat(update-data-bd): Creación de nuevas tablas

---
+++ README.md
@@ -0,0 +1,11 @@
+## Hello Developer
+
+I created this project in order to practice my mastery of Angular, Node and Docker technologies. I hope you like it 😀
+
+The way to get the entire project up and running is by entering the following command in your terminal:
+
+```sh
+sh start.sh
+```
+
+Enjoy this project that contains authorization and authentication of an app in which a table will be shown if you enter an Excel, CSV or JSON file in the format requested by the app.
---
+++ table/dump-auth_angular-202410251816.sql
@@ -0,0 +1,87 @@
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+--
+-- Host: localhost    Database: auth_angular
+-- ------------------------------------------------------
+-- Server version	9.0.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `fileData`
+--
+
+DROP TABLE IF EXISTS `fileData`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `fileData` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `type` varchar(100) DEFAULT NULL,
+  `data` text COMMENT 'Esta columna fue creada para guardar todos los datos que se ingresan al input de la página "table" del frontend',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `fileData_UN` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=97 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `fileData`
+--
+
+LOCK TABLES `fileData` WRITE;
+/*!40000 ALTER TABLE `fileData` DISABLE KEYS */;
+/*!40000 ALTER TABLE `fileData` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `lastname` varchar(60) NOT NULL,
+  `cellphone` varchar(40) NOT NULL,
+  `email` varchar(70) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `users_UN` (`email`),
+  CONSTRAINT `users_CHECK` CHECK (regexp_like(`email`,_utf8mb4'^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,}$'))
+) ENGINE=InnoDB AUTO_INCREMENT=65 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Luis','Saravia','941770750','saraviatornero@gmail.com','55555'),(2,'Luis','Saravia','981981981','saraviatornero310@gmail.com','9812379241'),(6,'Luis','Saravia','941770750','saraviatornero3104@gmail.com','9999'),(8,'Luis','Saravia','941770750','saraviatornero3105@gmail.com','9999'),(9,'Luis','Saravia','941770750','saraviatornero3106@gmail.com','9999'),(10,'Luis','Saravia','941770750','enriquesaravia310@gmail.com','enrique123'),(11,'Luis','Saravia','941770750','saraviatornero310+1@gmail.com','enrique123'),(12,'Luis','Saravia','941770750','saraviatornero310+2@gmail.com','Luis123123'),(13,'Luis','Saravia','941770750','saraviatornero310+3@gmail.com','Luis123123'),(63,'Luis','Saravia','941770750','saraviatornero310+4@gmail.com','Luis3333'),(64,'Luis','Saravia','941770750','saraviatornero310+5@gmail.com','Luis4444');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'auth_angular'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-10-25 18:16:24
